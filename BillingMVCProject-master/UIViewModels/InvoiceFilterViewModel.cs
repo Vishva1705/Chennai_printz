3d412a6267422629042cc9bb05169bbcfac7b808
Vishva D <114222222+Vishva1705@users.noreply.github.com>
2024-11-14 14:47:51 +0530

    update

--- BillingMVCProject-master/UIViewModels/InvoiceFilterViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BillingMVCProject.UIViewModels
-{
-    public class InvoiceFilterViewModel
-    {
-        public List<InvoiceCustomerDetail> Invoices { get; set; }
-        public DateTime SelectedDate { get; set; }
-        public string SearchTerm { get; set; }
-    }
-}
